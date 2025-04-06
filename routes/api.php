5de53a5065a176e44e65a9a2ad38e94ae85c0b78
Adrian <adrianmarrero18@gmail.com>
2025-04-06 19:23:29 +0200

    feat(login) - Create login route basic authentication

--- routes/api.php
+++ routes/api.php
@@ -10,3 +10,10 @@ Route::get('/user', function (Request $request) {
 Route::get('/health-check', function (Request $request) {
     return "ok";
 });
+
+Route::get('/login', function (Request $request) {
+    return response()->json([
+        'message' => 'Login successful',
+        'token' => '16546s-d4f6sdf4sdf-012aasdf-paslx15',
+    ]);
+});
